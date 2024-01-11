4e82ca460173acbcb522998c037f1f5a26a1eb6a
Aindrila Bhattacharjee <aindrilabhattacharjee72@gmail.com>
2024-01-11 20:03:44 +0530

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,8 +8,8 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "Next.js and Supabase Starter Kit",
-  description: "The fastest way to build apps with Next.js and Supabase",
+  title: "Images Gallery",
+  description: "unsplash clone Project",
 };
 
 export default function RootLayout({
